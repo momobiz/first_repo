8182a2f24c4e3ad19301439c7f1d357bcde03d61
momobiz <mohamedaliannabi35@gmail.com>
2020-09-21 11:09:00 +0200

    initialisation

---
+++ index1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<h1> titre </h1>
+	<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea modi eveniet harum. Labore sapiente eius, tempora harum assumenda facilis quidem illo consectetur, dolorem fuga non maiores eveniet dignissimos quibusdam dolorum.</p>
+</body>
+</html>
